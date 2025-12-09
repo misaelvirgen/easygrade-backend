33901fa6d6519c2c8725a70b9186831a16d5982d
Misael Virgen <misael.virgen@gmail.com>
2025-12-08 22:56:31 -0800

    Stripe initial setup

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ from routes.google_classroom import router as google_router
 from routes.canvas import router as canvas_router
 from routes import rubric
 from routes import upload_rubric
-
+from routes.stripe_webhook import router as stripe_webhook_router   # <-- ADD THIS
 
 app = FastAPI(title="EasyGrade Backend")
 
-# Allow frontend (Vercel) to call this API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # TODO: later restrict to your Vercel domain
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -31,3 +30,6 @@ app.include_router(google_router, prefix="/api")
 app.include_router(canvas_router, prefix="/api")
 app.include_router(rubric.router, prefix="/api/rubric")
 app.include_router(upload_rubric.router, prefix="/api/upload")
+
+# Stripe webhook route 
+app.include_router(stripe_webhook_router)
